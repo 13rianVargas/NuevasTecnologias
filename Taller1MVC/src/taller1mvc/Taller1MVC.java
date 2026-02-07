7497ae4aab34573c9d8dd9fdd6f9d8125783fd4f
13rianVargas <bsvargasc13@gmail.com>
2026-02-06 22:16:32 -0500

    chore/netbeans-base

---
+++ Taller1MVC/src/taller1mvc/Taller1MVC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller1mvc;
+
+/**
+ *
+ * @author 13rianvargas
+ */
+public class Taller1MVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
