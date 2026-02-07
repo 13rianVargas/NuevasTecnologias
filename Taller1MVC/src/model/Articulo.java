b0cb29685a573e39f181b54249e0e03ee7e3b696
Lina Bello <bellolina011@gmail.com>
2026-02-07 17:50:15 -0500

    Cambio "Libro" -> "Articulo"

---
+++ Taller1MVC/src/model/Articulo.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author dulce
+ */
+public class Articulo {
+    private int id; 
+    private String titulo; 
+    private String autores; 
+    private int anio;
+    private String revista;
+    private int volumen;
+    private int numero;     
+    private int paginas;
+    private double doi; 
+
+    public Articulo(String titulo, String autores, int anio, String revista, int volumen, int numero, int paginas, double doi) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.anio = anio;
+        this.revista = revista;
+        this.volumen = volumen;
+        this.numero = numero;
+        this.paginas = paginas;
+        this.doi = doi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getRevista() {
+        return revista;
+    }
+
+    public void setRevista(String revista) {
+        this.revista = revista;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public double getDoi() {
+        return doi;
+    }
+
+    public void setDoi(double doi) {
+        this.doi = doi;
+    }
+        
+}
