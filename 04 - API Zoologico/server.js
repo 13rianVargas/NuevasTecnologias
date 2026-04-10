2d3457b347d4495c29a9e58292ec90a80e80a40d
13rianVargas <bsvargasc13@gmail.com>
2026-04-09 23:00:44 -0500

    feat: add user authentication with bcrypt and auth routes

--- 04 - API Zoologico/server.js
+++ 04 - API Zoologico/server.js
@@ -1,29 +1,33 @@
-require("dotenv").config(); 
-const express = require("express"); 
-const mongoose = require("mongoose"); 
-const cors = require("cors"); 
- 
-const app = express(); 
- 
-// Middleware 
-app.use(cors());          // Permitir solicitudes CORS 
-app.use(express.json());  // Permitir recibir datos JSON en el cuerpo de la solicitud 
- 
-// Conectar a MongoDB 
-mongoose.connect(process.env.MONGO_URI) 
-  .then(() => console.log("MongoDB conectado")) 
-  .catch(err => console.error(err)); 
- 
-// Rutas 
-app.use("/api/animals", require("./routes/animalRoutes")); 
- 
-// Ruta base 
-app.get("/", (req, res) => { 
-  res.send("API funcionando 🚀"); 
-}); 
- 
-// Puerto de conexión 
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => { 
-  console.log(`Servidor corriendo en el puerto ${PORT}`); 
-}); 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authentication");
+const animalRoutes = require("./routes/animalRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Permitir solicitudes CORS
+app.use(express.json()); // Permitir recibir datos JSON en el cuerpo de la solicitud
+
+// Conectar a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error(err));
+
+// Rutas
+app.use("/api/animals", animalRoutes);
+app.use("/api/auth", authRoutes);
+
+// Ruta base
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+// Puerto de conexión
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
